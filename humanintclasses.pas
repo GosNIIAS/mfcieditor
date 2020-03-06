5bf1739c20a8bf835ea3c165d30bbf24a7b2d1e1
Лавренов Кирилл Евгеньевич <kelavrenov@voldemort-new.LAB70.2100.GOSNIIAS.LOCAL>
2020-03-06 13:29:56 +0300

    Файл классов для чел-маш интерфейса

---
+++ humanintclasses.pas
@@ -0,0 +1,23 @@
+unit HumanIntClasses;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  Format = class
+    private
+      var descript: string; //Определение микрозадачи для кадров
+      var
+
+
+  end;
+
+
+implementation
+
+end.
+
